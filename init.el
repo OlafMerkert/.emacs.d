2e2cb1308c738ddc9370efe7caa10c506554e0cb
Olaf Merkert <olaf@m-merkert.de>
2013-02-17 12:15:54 +0100

    Reverting "disable idle-highlight" Revert "disable idle-highlight"
    This reverts commit 07c670f004e6a2730d625e54479e37cc166f1f8f.

--- init.el
+++ init.el
@@ -49,7 +49,6 @@
 (require 'starter-kit-lisp)
 
 ;; disable some unwanted stuff from the starter kit
-(remove-hook 'prog-mode-hook 'idle-highlight-mode)
 
 ;; general settings
 (column-number-mode t)
@@ -69,6 +68,7 @@
 
 ;; don't flyspell automatically
 (remove-hook 'text-mode-hook 'turn-on-flyspell)
+
 
 (add-hook 'text-mode-hook
           (lambda ()
