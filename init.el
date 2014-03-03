ae6315b6fc7f8bd56d23d59fd76a87cc0a8807fa
Olaf Merkert <olaf@m-merkert.de>
2014-03-03 13:39:32 +0100

    allow using german dictionary for flyspell

--- init.el
+++ init.el
@@ -198,7 +198,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(safe-local-variable-values (quote ((Syntax . ANSI-Common-Lisp) (Package . emacs) (Package . HUNCHENTOOT) (Syntax . COMMON-LISP) (Base . 10) (Syntax . Common-Lisp) (Package . Maxima) (ispell-dictionary . "en_GB") (ispell-dictionary . "english") (whitespace-line-column . 80) (lexical-binding . t)))))
+ '(safe-local-variable-values (quote ((ispell-dictionary . "de") (Syntax . ANSI-Common-Lisp) (Package . emacs) (Package . HUNCHENTOOT) (Syntax . COMMON-LISP) (Base . 10) (Syntax . Common-Lisp) (Package . Maxima) (ispell-dictionary . "en_GB") (ispell-dictionary . "english") (whitespace-line-column . 80) (lexical-binding . t)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
