69abe368ad366f782c0dc5c8a20dadfab28a7bf0
Olaf Merkert <olaf@m-merkert.de>
2014-04-07 10:01:18 +0200

    configure org-capture (put TODO stuff in different files)

--- cnf-org.el
+++ cnf-org.el
@@ -1,14 +1,19 @@
 (provide 'cnf-org)
 
+(require 'org-capture)
+(require 'magit)
+
 (eval-after-load 'org
   '(progn
     (define-key org-mode-map (kbd "<f2>") 'insert-greek-letter)))
 
 (setq org-deadline-warning-days 5)
 (setq org-directory "~/Personal"
-      org-default-notes-file (concat org-directory "/notizen.org"))
+      org-default-notes-file (org-path "notizen"))
 
-(require 'magit)
+(defun org-path (filebase)
+   (concat org-directory "/" filebase ".org"))
+
 
 (defun autocommit-current-org-file ())
 
@@ -38,3 +43,44 @@
         (message "Commited changes to personal data.")))
     (magit-pull)
     (unless pull-only (magit-push))))
+
+;;; configure org capture
+(setq org-capture-my-todo-template "* TODO %?%i\n  %U\n  %a"
+      org-capture-templates
+      `(("t" "Todo" entry
+             (file+headline "" "Tasks")
+             ,org-capture-my-todo-template)
+        ("s" "SNS Todo" entry
+             (file+headline ,(org-path "sns") "Tasks")
+             ,org-capture-my-todo-template)
+        ("p" "Privat Todo" entry
+             (file+headline ,(org-path "privat") "Tasks")
+             ,org-capture-my-todo-template)
+        ;; TODO capture to readme.org of current (git) project
+        ("r" "Readme" entry
+             (function find-git-project-readme-tasks)
+             ,org-capture-my-todo-template)
+        ))
+
+(defun find-git-project-readme-tasks ()
+  (find-git-project-readme "Tasks"))
+
+(defun find-git-project-readme (&optional hd)
+  (interactive)
+  (let ((gitdir (magit-get-top-dir)))
+    (if gitdir
+        (progn
+          (find-file (concat gitdir "readme.org"))
+          (if hd
+              (if (re-search-forward
+                   (format org-complex-heading-regexp-format (regexp-quote hd))
+                   nil t)
+                  (goto-char (point-at-bol))
+                  (goto-char (point-max))
+                  (or (bolp) (insert "\n"))
+                  (insert "* " hd "\n")
+                  (beginning-of-line 0)
+                  (message "%s" (point))
+                  )))
+        (error "Not in a git project."))))
+
