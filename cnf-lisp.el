633fb83841d4b45e91e6474e2517c9f9cbd16cb3
Olaf Merkert <olaf@m-merkert.de>
2014-08-09 22:00:24 +0200

    attempt to provide better indentation for cl-who html code

--- cnf-lisp.el
+++ cnf-lisp.el
@@ -188,6 +188,9 @@
                      (dbind destructuring-bind)
                      )
 
+;; nicer indentation for cl-who (at least for common html tags)
+(mapc (lambda (tag) (setf (get tag 'common-lisp-indent-function) '(&body)))
+      '(:a :abbr :acronym :address :applet :area :article :aside :audio :b :base :basefont :bdi :bdo :big :blockquote :body :br :button :canvas :caption :center :cite :code :col :colgroup :command :datalist :dd :del :details :dfn :dir :div :dl :dt :em :embed :fieldset :figcaption :figure :font :footer :form :frame :frameset :h1 :h2 :h3 :h4 :h5 :h6 :head :header :hgroup :hr :html :i :iframe :img :input :ins :keygen :kbd :label :legend :li :link :map :mark :menu :meta :meter :nav :noframes :noscript :object :ol :optgroup :option :output :p :param :pre :progress :q :rp :rt :ruby :s :samp :script :section :select :small :source :span :strike :strong :style :sub :summary :sup :table :tbody :td :textarea :tfoot :th :thead :time :title :tr :track :tt :u :ul :var :video :wbr))
 
 (defadvice common-lisp-hyperspec (around hyperspec-use-w3m)
   "Use w3m as for `browse-url' for the CL hyperspec."
