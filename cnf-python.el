660298e5a9e0670d717634cac9fe95ccc50d2121
Olaf Merkert <olaf@m-merkert.de>
2014-09-03 15:15:59 +0200

    some functionality to conveniently only send current line or definition to the python interpreter

--- cnf-python.el
+++ cnf-python.el
@@ -31,4 +31,38 @@
 ;; return and pass removes one level, while double blank lines reset
 ;; indentation to 0
 
+(setq python-shell-interpreter "sage"
+      python-shell-interpreter-args "-python -i")
+
+(defun elpy-shell-send-line ()
+  "Send the current line to the Python shell. "
+  (interactive)
+  ;; Ensure process exists
+  (elpy-shell-get-or-create-process)
+  (let ((region (elpy-shell--region-without-indentation
+                 (save-excursion (beginning-of-line) (point))
+                 (save-excursion (end-of-line) (point)))))
+    (python-shell-send-string region))
+  (display-buffer (process-buffer (elpy-shell-get-or-create-process))
+                  nil
+                  'visible))
+
+(defun elpy-shell-send-definition ()
+  "Send the part of the buffer from current point up to the beginning of the current toplevel form"
+  (interactive)
+  ;; Ensure process exists
+  (elpy-shell-get-or-create-process)
+  (let ((region (elpy-shell--region-without-indentation
+                 (save-excursion
+                   (search-backward-regexp "^\\(def\\|class\\)")
+                   (point))
+                 (point))))
+    (python-shell-send-string region))
+  (display-buffer (process-buffer (elpy-shell-get-or-create-process))
+                  nil
+                  'visible))
+
+(define-key python-mode-map (kbd "C-x C-e") 'elpy-shell-send-line)
+(define-key python-mode-map (kbd "C-M-x") 'elpy-shell-send-definition)
+
 (provide 'cnf-python)
