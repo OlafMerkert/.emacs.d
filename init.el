c7bdfe330a3fdc8c4877b6c5d4b89cd8744c4a22
Olaf Merkert <olaf@m-merkert.de>
2014-09-29 14:54:45 +0200

    mark italian safe as dictionary language

--- init.el
+++ init.el
@@ -178,7 +178,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files (quote ("~/Personal/bookmarks.org" "~/Projekte/web-utils/readme.org" "~/Personal/spiele.org" "~/Personal/sns.org" "~/Personal/notizen.org" "~/Personal/privat.org" "~/Perfezionamento/thesis/phd-thesis.en.org")))
- '(safe-local-variable-values (quote ((Syntax . ANSI-Common-Lisp) (Syntax . COMMON-LISP) (Base . 10) (Syntax . Common-Lisp) (ispell-dictionary . "de") (ispell-dictionary . "en_GB") (ispell-dictionary . "english") (whitespace-line-column . 80) (lexical-binding . t) (org-babel-python-command . "sage -python")))))
+ '(safe-local-variable-values (quote ((ispell-dictionary . "it") (Syntax . ANSI-Common-Lisp) (Syntax . COMMON-LISP) (Base . 10) (Syntax . Common-Lisp) (ispell-dictionary . "de") (ispell-dictionary . "en_GB") (ispell-dictionary . "english") (whitespace-line-column . 80) (lexical-binding . t) (org-babel-python-command . "sage -python")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
