ede3c12dfd49ac4a59a442aba9fe0febf4c35f58
Olaf Merkert <olaf@m-merkert.de>
2015-03-29 14:55:22 +0200

    customise ibuffer columns

--- config/cnf-base.el
+++ config/cnf-base.el
@@ -65,6 +65,42 @@
   (define-key ibuffer-mode-map (kbd "C-x C-b") 'ibuffer-vc-set-filter-groups-by-vc-root)
   (define-key ibuffer-mode-map (kbd "g") 'ibuffer-vc-set-filter-groups-by-vc-root))
 
+;; Use human readable Size column instead of original one
+(after-load 'ibuffer
+  (define-ibuffer-column size-h (:name "Size" :inline t)
+    (cond ((> (buffer-size) 1000000000) (format "%7.1fG" (/ (buffer-size) 1000000000.0)))
+          ((> (buffer-size) 1000000) (format "%7.1fM" (/ (buffer-size) 1000000.0)))
+          ((> (buffer-size) 1000) (format "%7.1fk" (/ (buffer-size) 1000.0)))
+          (t (format "%8d" (buffer-size))))))
+
+(setq ibuffer-formats
+      '((mark modified read-only " "
+         (name 42 42 :left :elide)
+         " "
+         (size-h 9 -1 :right)
+         " "
+         filename-and-process)
+        (mark modified read-only vc-status-mini " "
+         (name 20 20 :left :elide)
+         " "
+         (size-h 9 -1 :right)
+         " "
+         (mode 16 16 :left :elide)
+         " "
+         filename-and-process)
+        (mark modified read-only vc-status-mini " "
+         (name 20 20 :left :elide)
+         " "
+         (size-h 9 -1 :right)
+         "  "
+         (vc-status 16 16 :left)
+         " "
+         filename-and-process)
+        (mark modified read-only " "
+         (name 30 -1 :left)
+         " "
+         filename-and-process)))
+
 ;;; some keybindings
 (global-set-key (kbd "S-<return>") 'split-line)
 (global-set-key (kbd "C-x M-o")    'other-frame)
