807e3c02b951e0abd1f94786bb938197786494d2
Olaf Merkert <olaf@m-merkert.de>
2015-08-06 17:36:56 +0200

    reenable ibuffer-vc

--- config/cnf-vc.el
+++ config/cnf-vc.el
@@ -7,24 +7,25 @@
           magit-push-always-verify nil))
 
 (use-package ibuffer-vc
-    :disabled t
-    :init (progn (require 'vc)
-                 (add-hook 'ibuffer-hook 'ibuffer-vc-set-filter-groups-by-vc-root))
+    :disabled nil
+    :init
+    (progn
+      (require 'vc)
+      (define-key ibuffer-mode-map (kbd "C-x C-b") 'ibuffer-vc-set-filter-groups-by-vc-root)
+      (define-key ibuffer-mode-map (kbd "g") 'ibuffer-vc-set-filter-groups-by-vc-root))
     :commands (ibuffer-vc-set-filter-groups-by-vc-root)
     :config
-    (defun ibuffer-vc-set-filter-groups-by-vc-root ()
-      "Set the current filter groups to filter by vc root dir."
-      (interactive)
-      (setq ibuffer-filter-groups (ibuffer-vc-generate-filter-groups-by-vc-root))
-      (message "ibuffer-vc: groups set")
-      (let ((ibuf (get-buffer "*Buffer List*")))
-        (when ibuf
-          (with-current-buffer ibuf
-            (pop-to-buffer ibuf)
-            (ibuffer-update nil t))))))
-
-(after-load 'ibuffer-vc
-  (define-key ibuffer-mode-map (kbd "C-x C-b") 'ibuffer-vc-set-filter-groups-by-vc-root)
-  (define-key ibuffer-mode-map (kbd "g") 'ibuffer-vc-set-filter-groups-by-vc-root))
+    (progn
+      (defun ibuffer-vc-set-filter-groups-by-vc-root ()
+        "Set the current filter groups to filter by vc root dir."
+        (interactive)
+        (setq ibuffer-filter-groups (ibuffer-vc-generate-filter-groups-by-vc-root))
+        (message "ibuffer-vc: groups set")
+        (let ((ibuf (get-buffer "*Buffer List*")))
+          (when ibuf
+            (with-current-buffer ibuf
+              (pop-to-buffer ibuf)
+              (ibuffer-update nil t)))))
+      (add-hook 'ibuffer-hook 'ibuffer-vc-set-filter-groups-by-vc-root)))
 
 (provide 'cnf-vc)
