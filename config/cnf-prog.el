d24ca34289fb10ecdf9b7100189a3af780e0b1fc
Olaf Merkert <olaf@m-merkert.de>
2015-08-12 18:49:47 +0200

    enable some additional javascript functionality

--- config/cnf-prog.el
+++ config/cnf-prog.el
@@ -12,13 +12,37 @@
 (setq js-indent 4)
 
 (defun esk-pretty-function ()
-  (font-lock-add-keywords nil `(("(\\(function *\\)"
-                                 (0 (progn (compose-region (match-beginning 1)
-                                                           (match-end 1)
-                                                           "\u0192"
-                                                           'decompose-region)))))))
+  (font-lock-add-keywords
+   nil `(("\\(function *\\) *("
+          (0 (progn (compose-region (match-beginning 1)
+                                    (match-end 1)
+                                    "\u0192"
+                                    'decompose-region)))))))
 
-(add-hook 'js-mode 'esk-pretty-function)
+(add-hook 'js-mode-hook 'esk-pretty-function)
+
+(use-package ac-js2
+    :ensure t
+    :config
+    (add-hook 'js2-mode-hook 'ac-js2-mode)
+    (setq ac-js2-evaluate-calls t))
+
+;; (add-to-list 'ac-js2-external-libraries "path/to/lib/library.js")
+
+(use-package js-comint
+    :ensure t
+    :config
+    (setenv "NODE_NO_READLINE" "1")
+    (setq inferior-js-program-command "/usr/bin/node")
+    (defun install-js-comint-bindings ()
+      (local-set-key "\C-x\C-e" 'js-send-last-sexp)
+      (local-set-key "\C-\M-x" 'js-send-last-sexp-and-go)
+      (local-set-key "\C-cb" 'js-send-buffer)
+      (local-set-key "\C-c\C-b" 'js-send-buffer-and-go)
+      (local-set-key "\C-cl" 'js-load-file-and-go))
+    (add-hook 'js-mode-hook 'install-js-comint-bindings))
+
+(use-package json-mode :ensure t)
 
 (use-package lua-mode :ensure t :commands lua-mode)
 
