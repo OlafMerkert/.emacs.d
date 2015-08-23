25e6a1b3442664256a675d7c3dd2e533dec9664b
Olaf Merkert <olaf@m-merkert.de>
2015-08-23 22:37:04 +0200

    remove superfluous paredit activation

--- config/cnf-colours.el
+++ config/cnf-colours.el
@@ -35,8 +35,6 @@
 (dolist (mode '(scheme emacs-lisp lisp clojure clojurescript))
     (when (> (display-color-cells) 8)
       (font-lock-add-keywords (intern (concat (symbol-name mode) "-mode"))
-                              '(("(\\|)" . 'esk-paren-face))))
-    (add-hook (intern (concat (symbol-name mode) "-mode-hook"))
-              'paredit-mode))
+                              '(("(\\|)" . 'esk-paren-face)))))
 
 (provide 'cnf-colours)
