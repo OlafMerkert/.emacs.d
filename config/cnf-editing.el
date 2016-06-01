bfd27ef9a8395d8bde7a02fcc9e3720f310f98f8
Olaf Merkert <olaf@m-merkert.de>
2016-06-01 18:11:12 +0200

    make fill-paragraph smarter!
    from endlessparentheses

--- config/cnf-editing.el
+++ config/cnf-editing.el
@@ -88,4 +88,19 @@ TeX."
 
 (global-set-key (kbd "C-%") 'region-query-replace)
 
+;; from http://endlessparentheses.com/fill-and-unfill-paragraphs-with-a-single-key.html
+(defun endless/fill-or-unfill ()
+  "Like `fill-paragraph', but unfill if used twice."
+  (interactive)
+  (let ((fill-column
+         (if (eq last-command 'endless/fill-or-unfill)
+             (progn (setq this-command nil)
+                    (point-max))
+           fill-column)))
+    (call-interactively #'fill-paragraph)))
+
+(global-set-key [remap fill-paragraph]
+                #'endless/fill-or-unfill)
+
+
 (provide 'cnf-editing)
