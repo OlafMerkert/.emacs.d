907bcd2d5749aaea432229f632d8a1cd287cabfd
Olaf Merkert <olaf@m-merkert.de>
2016-06-23 17:37:48 +0200

    ensure nautilus-python extension dir exists

--- external/setup-nautilus.sh
+++ external/setup-nautilus.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+mkdir -p $HOME/.local/share/nautilus-python/extensions/
 ln -sf $HOME/.emacs.d/external/nautilus_org_integration.py $HOME/.local/share/nautilus-python/extensions/org_integration.py
